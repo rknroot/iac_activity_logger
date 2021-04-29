95d072de6b2dfd42964347f1f71aebf67a7d7a8d
RK <rknroot@gmail.com>
2021-04-29 23:23:42 +0530

    Field updates

--- iac_activity_logger/iac_activity_logger/doctype/tasks_settings/tasks_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Eco Data & IAC and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Tasks Settings', {
-	// refresh: function(frm) {
-
-	// }
-});
